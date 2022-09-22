d5f8d818ad4ff55b54191a9e6ac5eefdc213e6a5
BeshtaNazar <weirddisaster1@gmail.com>
2022-09-22 12:54:04 +0300

    initial commit

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    void Start()
+    {
+        //You can access a static variable by using the class name
+        //and the dot operator.
+        int x = Player.playerCount;
+        Debug.Log(x);
+    }
+}
